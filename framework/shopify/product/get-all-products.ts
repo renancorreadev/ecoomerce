f9b75fb7f06ac40d636b8e241030bdef8ab88c45
skyxcripto <producer@skynance.net>
2022-01-20 23:55:46 -0300

    feat: create connection to api shopify

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,10 +1,19 @@
-const fetchApi = async () => {
-  const url = 'https://jsonplaceholder.typicode.com/todos'
+import getAllProductsQuery from '../utils/queries/get-all-products'
+
+type FetchParams = {
+  query: string
+}
+
+const fetchApi = async ({ query }: FetchParams) => {
+  const url = 'http://localhost:4000/graphql'
   const result = await fetch(url, {
-    method: 'GET',
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
+    body: JSON.stringify({
+      query,
+    }),
   })
 
   const data = await result.json()
@@ -12,7 +21,7 @@ const fetchApi = async () => {
 }
 
 const getAllProducts = async (): Promise<any[]> => {
-  const products = await fetchApi()
+  const products = await fetchApi({ query: getAllProductsQuery })
   return products.data
 }
 
