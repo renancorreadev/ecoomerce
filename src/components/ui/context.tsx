273e3956e4bdeade5adfb2db32b52542444a46fa
skyxcripto <producer@skynance.net>
2022-01-29 19:04:57 -0300

    feat: finished openSidebar & CloseSidebar to MyCart

--- src/components/ui/context.tsx
+++ src/components/ui/context.tsx
@@ -1,4 +1,4 @@
-import { createContext, FC, useContext, useState } from 'react'
+import { createContext, FC, useContext, useReducer } from 'react'
 export interface StateModifiers {
   openSidebar: () => void
   closeSidebar: () => void
@@ -21,14 +21,35 @@ const UIContext = createContext<State>({
   ...initialState,
 })
 
+type Action = { type: 'OPEN_SIDEBAR' | 'CLOSE_SIDEBAR' }
+
+function uiReducer(state: StateValues, action: Action) {
+  switch (action.type) {
+    case 'OPEN_SIDEBAR': {
+      return {
+        ...state,
+        isSidebarOpen: true,
+      }
+    }
+    case 'CLOSE_SIDEBAR': {
+      return {
+        ...state,
+        isSidebarOpen: false,
+      }
+    }
+  }
+}
+
 export const UIProvider: FC = ({ children }) => {
-  const openSidebar = () => alert('Opening')
-  const closeSidebar = () => alert('Closing')
+  const [state, dispatch] = useReducer(uiReducer, initialState)
+
+  const openSidebar = () => dispatch({ type: 'OPEN_SIDEBAR' })
+  const closeSidebar = () => dispatch({ type: 'CLOSE_SIDEBAR' })
 
   const value = {
+    ...state,
     openSidebar,
     closeSidebar,
-    isSidebarOpen: false,
   }
   return (
     <>
