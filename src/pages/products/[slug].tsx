602514dcc91c3c3c63d8671bfce46105c7ebc6c7
skyxcripto <producer@skynance.net>
2022-01-31 10:20:30 -0300

    feat": develop slug.tsx to products params  ✅

--- src/pages/products/[slug].tsx
+++ src/pages/products/[slug].tsx
@@ -1,13 +1,39 @@
 import { Layout } from '@components/common'
+import {
+  GetStaticPaths,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+} from 'next'
 
 //fetch all products slugs
-export const getStaticPaths = () => {}
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { slug: 'cool-hat' } },
+      { params: { slug: 't-shirt' } },
+      { params: { slug: 'lightweigth-jacket' } },
+    ],
+    fallback: false,
+  }
+}
 
 //provide product specific data to the page
-export const getStaticProps = () => {}
+export const getStaticProps = async ({
+  params,
+}: GetStaticPropsContext<{ slug: string }>) => {
+  return {
+    props: {
+      product: {
+        slug: params?.slug,
+      },
+    },
+  }
+}
 
-export default function ProductSlug() {
-  return <div>Hello</div>
+export default function ProductSlug({
+  product,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <div>{product.slug}</div>
 }
 
 ProductSlug.Layout = Layout
